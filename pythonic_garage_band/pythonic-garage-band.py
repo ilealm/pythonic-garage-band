b777773a2e6c5e49a435f8c925b5ad54d3e61ddc
Iris Leal <iris_leal_m@hotmail.com>
2020-05-14 16:16:06 -0700

    Change on structure

--- pythonic_garage_band/pythonic-garage-band.py
+++ pythonic_garage_band/pythonic-garage-band.py
@@ -1,22 +1,57 @@
 class Musician:
-    def __init__(self, name, instrument):
+    def __init__(self, name):
         self.name = name
-        self.instrument = instrument
 
     def __repr__(self):
-        return {'name':self.name, 'instrument':self.instrument}
+      return {'name':self.name}
 
     def __str__(self):
-        return 'Musician(name='+self.name+', instrument='+self.instrument+ ')'
+        return 'Musician(name='+self.name+')'
+    
+
+
+class Guitarist(Musician):
+    def __init__(self, name):
+        self.name = name
+        self.instrument = "Guitar"
     
     def get_instrument(self):
-        return self.instrument
+        return "Guitar"
+
+    def play_solo(self):
+        return f"{self.name} is playing an amazing {self.instrument} solo!!!"
+    
+    def __repr__(self):
+        return self
+
+    def __str__(self):
+        return self
 
 
+class Band:
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return self
+
+    def __str__(self):
+        return self
+    
+
+    def play_solos(self):
+        print("play solos")
+    #     for i in len(self.members):
+    #         print(self.members[i])
 
 
-
+ 
 if __name__ == "__main__":
-    Guitarist = Musician("Mary", "Guitar")
-    print (Guitarist.name)
-    print(Guitarist.get_instrument())
+    my_guitarist = Guitarist("Carlos Santanna")
+
+    my_band = Band("Apocaliptica")
+
+    print( my_guitarist.play_solo())
+    print(my_band.name)
+
+    # my_band = Band("I like coffee")
